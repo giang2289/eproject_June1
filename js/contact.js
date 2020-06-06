e0ed316482d5a40b3255d99b8a3dd3d09b67c990
c1909i2vanh <truonggiang2298@gmail.com>
2020-06-06 22:27:23 +0700

    edit page contact

--- js/contact.js
+++ js/contact.js
@@ -6,7 +6,7 @@ r(function(){
 });
 var mapObj = null;
 var defaultCoord = [10.7743, 106.6669]; // coord mặc định, 9 giữa HCMC
-var zoomLevel = 13;
+var zoomLevel = 14;
 var mapConfig = {
     attributionControl: false, // để ko hiện watermark nữa
     center: defaultCoord, // vị trí map mặc định hiện tại
@@ -15,11 +15,24 @@ var mapConfig = {
 
 window.onload = function() {
     // init map
-    mapObj = L.map('map', {attributionControl: false}).setView(defaultCoord, zoomLevel);
     
+    mapObj = L.map('map', {attributionControl: false}).setView(defaultCoord, zoomLevel);
+    // tạo marker
+    // tạo marker
+
+
+		var marker = L.marker([10.7743, 106.6669]).addTo(mapObj).on('click',function(e){console.log(this)});
+        var marker1 = L.marker([10.7800, 106.6669]).addTo(mapObj).on('click',function(e){alert(this.getLatLng())});
+		// tạo popup và gán vào marker vừa tạo
+		var popup = L.popup();
+		popup.setContent("<b>Seth Phát</b> <br> Hello World Nà");
+        marker.bindPopup(popup);
+   
     // add tile để map có thể hoạt động, xài free từ OSM
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapObj);
+    
+ 
 };
 
