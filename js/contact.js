7aa08b3799c4e4fc13d8cebcedacc485c1320509
c1909i2vanh <truonggiang2298@gmail.com>
2020-06-06 17:40:14 +0700

    fix bootstrap

--- js/contact.js
+++ js/contact.js
@@ -4,20 +4,22 @@ function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
 r(function(){
    
 });
+var mapObj = null;
+var defaultCoord = [10.7743, 106.6669]; // coord mặc định, 9 giữa HCMC
+var zoomLevel = 13;
+var mapConfig = {
+    attributionControl: false, // để ko hiện watermark nữa
+    center: defaultCoord, // vị trí map mặc định hiện tại
+    zoom: zoomLevel, // level zoom
+};
 
-r(function(exports) {
-    "use strict";
+window.onload = function() {
+    // init map
+    mapObj = L.map('map', {attributionControl: false}).setView(defaultCoord, zoomLevel);
+    
+    // add tile để map có thể hoạt động, xài free từ OSM
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapObj);
+};
 
-    function initMap() {
-        var map;
-      exports.map = new google.maps.Map(document.getElementById("map"), {
-        center: {
-          lat: -34.397,
-          lng: 150.644
-        },
-        zoom: 8
-      });
-    }
-
-    exports.initMap = initMap;
-  })((this.window = this.window || {}));
